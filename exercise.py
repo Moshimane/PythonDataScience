c25293cf697ae67088c3c365980b392166280765
Moshimane <joy.moshimane@gmail.com>
2024-04-01 18:55:52 +0200

    python refresher part 1

---
+++ exercise.py
@@ -0,0 +1,28 @@
+value = input('Enter number greater than 1')
+num = int(value)
+
+def printtriangle(num):
+    for i in range(num+1):
+        for j in range(i):
+            print('*', end="")
+        print()
+        
+def calculate_area(a,b,shape):
+    if shape == 'triangle':
+        area = (a*b)/2
+        print(f'Area of {a}x{b} triangle is {area}')
+    elif shape == 'circle':
+        area = 3.14*a*a
+        print(f'Area of {a}x{a} circle is {area}')
+    elif shape =='rectangle':
+        area = a*b
+        print(f'Area of {a}x{b} rectangle is {area}')
+    else:
+        print(f'Invalid shape {shape}')
+        
+printtriangle(num)
+
+num2 = int(input('Enter positive number'))
+shape = input('Enter name of shape')
+
+calculate_area(num,num2,shape)
---
+++ play-around.py
@@ -0,0 +1,125 @@
+#                      Removing the Py Rust               #
+#                                                         #
+###########################################################
+
+
+###########################################################
+#                      Strings                            #
+###########################################################
+
+string = 'i am coding in python'
+print(string)
+
+#string[0] = 'i'  : this throws a type error because str object does not support item assignment which means strings are immutable
+
+#print substring to a specific index
+print(string[0:5])
+
+#print substring from index to the rest of the string
+print(string[5:])
+
+#use triple codes for multi line string
+
+string = """Start
+i am coding in python
+End"""
+print(string)
+
+#another method is to use \n
+string = 'start2\nI am coding in python\nEnd2'
+print(string)
+
+#convert number to a string and do concatination
+string = 'I am coding in python '
+num = 2
+
+print(string + str(num))
+
+##########################################################
+#                       Lists                            #
+##########################################################
+
+numbers = [1, 2, 3, 4]
+print(numbers)
+
+print(numbers[0])
+
+#print sub list
+print(numbers[0:2])
+
+#print rest of the list
+print(numbers[2:])
+
+#add/append item to list -- add 5
+numbers.append(5)
+print(numbers)
+
+#add at a specific location -- add 0
+numbers.insert(0,0)
+print(numbers)
+
+bigNumbers = [100,101,102,103,104]
+
+#join lists
+combinedNumbers = numbers + bigNumbers
+print(combinedNumbers)
+
+#print length of a list
+print(len(combinedNumbers))
+
+#check if number exists in list
+print(100 in combinedNumbers)
+
+
+##########################################################
+#                   If Statement                         #
+##########################################################
+
+if 100 in combinedNumbers:
+    print('100 exists in list')
+else: print("100 doesn't exists in list")
+
+value = input("Enter a number")
+num = int(value)
+
+if num == 0:
+    print("Zero")
+elif num == 1:
+    print("One")
+elif num >= 2:
+    print("Greater than one")
+elif num < 0 and num%2==0:
+    print("Negative even number")
+else:
+    print("Negative odd number")
+    
+
+##########################################################
+#                       For Loop                         #
+########################################################## 
+
+for i in combinedNumbers:
+    if i%2==0:
+        print(i, "is even number")
+
+#specific number incremental
+for i in range(0, 10, 2):
+    print(i)
+    
+    
+#range with length
+for i in range(len(combinedNumbers)):
+    print(combinedNumbers[i])
+    
+    
+##########################################################
+#                       Methods                          #
+########################################################## 
+
+def DoSomething():
+    print("I am doing something")
+    
+DoSomething()
+
+def calculateSum(a,b):
+    return a+b
